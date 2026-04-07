c84a4bada93b022c3d7dada9b09c24ebe2f85519
groot <nomail@nomail.com>
2026-04-07 14:02:42 +0530

    getLanguages and getStatuses added

--- client.go
+++ client.go
@@ -1,9 +1,73 @@
 package judge0
 
-import "net/http"
+import (
+	"net/http"
+	"fmt"
+	"encoding/json"
+	)
+
+
+
+
+type Language struct {
+	Id int
+	Name string
+}
+
+type Status struct {
+	Id int
+	Description string
+
+}
+
+
+
+
 
 type Client struct {
 	authProvider AuthProvider
 	httpClient   *http.Client
 	baseURL      string
+}
+
+
+
+//returns a slice of all supported languages by judge0
+func (c *Client) getLanguages() ([]Language, error){
+	
+	
+	data, err := c.doRequest(c.baseURL+"/languages", "GET", nil)
+
+	if err!=nil{
+		return nil, fmt.Errorf("failed to get languages: %v", err)
+	}
+
+	var languages []Language
+	err = json.Unmarshal(data, &languages)
+	if err!=nil{
+		return nil, fmt.Errorf("failed to serialize/Unmarshal languages: %v", err)
+	}
+
+	return languages, nil
+
+}
+
+//returns a slice of all the judge0 status codes
+func (c *Client) getStatuses() ([]Status, error){
+
+	data, err := c.doRequest(c.baseURL+"/statuses", "GET", nil)
+
+	if err!=nil{
+		return nil, fmt.Errorf("failed to get statuses: %v", err)
+	}
+
+	
+	var statuses []Status
+	err = json.Unmarshal(data, &statuses)
+	if err!=nil{
+		return nil, fmt.Errorf("failed to serialize/Unmarshal statuses: %v", err)
+	}
+
+	return statuses, nil
+
 }
